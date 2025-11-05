3491520cf0c7b87ec475cdc9fd647783815894a2
lychaoaa <63580233+lychaoaa@users.noreply.github.com>
2025-11-05 17:21:07 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
+let lightOn = false
 input.onSound(DetectedSound.Loud, function () {
-	
+    lightOn = !(lightOn)
+    if (true) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.clearScreen()
+    }
 })
 basic.forever(function () {
 	
